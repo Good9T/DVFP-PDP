d7d60db36a2a0030d07409446e9f9eefd9b9caca
Good9T <46553608+Good9T@users.noreply.github.com>
2026-04-29 11:25:25 +0800

    Add files via upload

---
+++ src/PDTSP/euclidean/PDSNO/test20.py
@@ -0,0 +1,105 @@
+#########################################################################################
+# Machine Environment Config
+
+DEBUG_MODE = False
+USE_CUDA = not DEBUG_MODE
+CUDA_DEVICE_NUM = 0
+
+##########################################################################################
+# Path Config
+
+import os
+import sys
+
+os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
+os.environ["OMP_NUM_THREADS"] = "1"
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+sys.path.insert(0, "../../..")
+sys.path.insert(0, "../../../..")
+
+##########################################################################################
+# import
+
+import logging
+
+from utils import create_logger
+
+from PDTSP_Tester import PDTSPTester as Tester
+
+##########################################################################################
+# parameters
+
+env_params = {
+    'customer_size': 10,
+    'node_size': 21,
+    'problem_gen_params': {
+        'scaler': 1.0,
+    },
+    'pomo_size': 10,
+}
+
+model_params = {
+    'embedding_dim': 256,
+    'sqrt_embedding_dim': 256 ** 0.5,
+    'encoder_layer_num': 5,
+    'qkv_dim': 16,
+    'sqrt_qkv_dim': 16 ** 0.5,
+    'head_num': 16,
+    'logit_clipping': 10,
+    'ff_hidden_dim': 512,
+    'ms_hidden_dim': 16,
+    'ms_layer1_init': (1 / 2) ** 0.5,
+    'ms_layer2_init': (1 / 16) ** 0.5,
+    'eval_type': 'argmax',
+    'one_hot_seed_num': 150,
+}
+
+tester_params = {
+    'use_cuda': USE_CUDA,
+    'cuda_device_num': CUDA_DEVICE_NUM,
+    'model_load': {
+        'path': './result/train20',  # directory path of pre-trained model and log files saved.
+        'epoch': 2000,  # epoch version of pre-trained model to laod.
+    },
+    'test_episodes':2000,
+    'test_batch_size': 100,
+    'augmentation_enable': True,
+    'aug_factor': 50,
+    'aug_batch_size': 50,
+}
+if tester_params['augmentation_enable']:
+    tester_params['test_batch_size'] = tester_params['aug_batch_size']
+
+logger_params = {
+    'log_file': {
+        'desc': 'PDSNO_PDTSP20_test',
+        'filename': 'log.txt'
+    }
+}
+
+
+##########################################################################################
+# main
+
+def main():
+    create_logger(**logger_params)
+    _print_config()
+
+    tester = Tester(env_params=env_params,
+                    model_params=model_params,
+                    tester_params=tester_params)
+
+    tester.run()
+
+
+def _print_config():
+    logger = logging.getLogger('root')
+    logger.info('DEBUG_MODE: {}'.format(DEBUG_MODE))
+    logger.info('USE_CUDA: {}, CUDA_DEVICE_NUM: {}'.format(USE_CUDA, CUDA_DEVICE_NUM))
+    [logger.info(f"{g_key} = {globals()[g_key]}") for g_key in globals() if g_key.endswith('params')]
+
+
+##########################################################################################
+
+if __name__ == "__main__":
+    main()
